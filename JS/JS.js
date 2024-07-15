5465feca1d1cd6a6f22872dcaafdc5f9aebdb37e
RanjeethAcharya <ranjeetkumar933342@gmail.com>
2024-07-15 18:13:02 +0530

    js

---
+++ JS/JS.js
@@ -0,0 +1,12 @@
+// console.log(`hello`);
+// console.log(`i like nun`);
+
+// window.alert(`this is an alert`);
+// window.alert(`I like nun`);
+
+// this is a comment
+
+document.getElementById(`h1`).textContent = `hello`;
+document.getElementById(`p1`).textContent = `nun`; 
+
+
